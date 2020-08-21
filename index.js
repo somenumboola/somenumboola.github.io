a793add2f6517255adf7b95ee3a37ead6a32eb6f
Tony <40070247+desulaid@users.noreply.github.com>
2020-08-21 21:52:41 +0400

    файлспавер

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+  profiles: [
+    { name: 'VK', url: 'https://vk.com/id152967894'},
+    { name: 'Telegram', url: 'https://t.me/hochupoebachu'},
+    { name: 'Instagram', url: 'https://www.instagram.com/desulaid/?hl=ru'},
+    { name: 'Spotify', url: 'https://open.spotify.com/user/2uq5uxac9mzr6yadjsjr8fk1f?si=luMO22GZR8KGQIe3J5ek4g'},
+    { name: 'and Github (:', url: 'https://github.com/desulaid'},
+  ]
+}});
