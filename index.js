3502c0bd43f9f2398d32dd9ac9e9f83790bf949b
Tony <40070247+desulaid@users.noreply.github.com>
2020-08-21 22:32:23 +0400

    add tiktok

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = new Vue({
     { name: 'Telegram', url: 'https://t.me/hochupoebachu'},
     { name: 'Instagram', url: 'https://www.instagram.com/desulaid/?hl=ru'},
     { name: 'Spotify', url: 'https://open.spotify.com/user/2uq5uxac9mzr6yadjsjr8fk1f?si=luMO22GZR8KGQIe3J5ek4g'},
+    { name: 'TikTok', url: 'https://www.tiktok.com/@desulaid?lang=ru'},
     { name: 'and Github (:', url: 'https://github.com/desulaid'},
   ]
 }});
