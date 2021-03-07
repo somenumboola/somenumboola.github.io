2b2588fdc7fe2c1db92df8a4289891840b86d57a
Alexander Glushchenko <somenumboola@gmail.com>
2021-03-07 14:04:59 +0200

    Re-order links

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var app = new Vue({
   el: '#app',
   data: {
   profiles: [
-    { name: 'VK', url: 'https://vk.com/somenumboola', hover: false},
     { name: 'Telegram', url: 'https://t.me/somenumboola', hover: false},
     { name: 'Instagram', url: 'https://www.instagram.com/inredrum/?hl=ru', hover: false},
     { name: 'Spotify', url: 'https://open.spotify.com/user/31lx7juvjj654lya32fldt7eevla?si=spbkueYPRtmc99Q1cip2vQ', hover: false},
     { name: 'TikTok', url: 'https://www.tiktok.com/@somenumboola?lang=ru', hover: false},
     { name: 'Savee.IT', url: 'https://savee.it/somenumboola/', hover: false},
     { name: 'ClubHouse', url: 'https://clubber.one/@somenumboola', hover: false},
+    { name: 'VK', url: 'https://vk.com/somenumboola', hover: false},
     { name: 'and Github (:', url: 'https://github.com/desulaid', hover: false}
   ]
 }});
